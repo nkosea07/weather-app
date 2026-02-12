58b9438a33d60573ac88af8119aa1febe3e936b9
nkosea07 <nkosiphile@marytechenock.com>
2026-02-12 23:15:03 +0200

    Temporarily open API security and improve root handling

--- backend/src/main/java/com/uzwide/WeatherApp/WeatherAppApplication.java
+++ backend/src/main/java/com/uzwide/WeatherApp/WeatherAppApplication.java
@@ -2,10 +2,11 @@ package com.uzwide.WeatherApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 @EnableCaching
 @EnableScheduling
 public class WeatherAppApplication {
