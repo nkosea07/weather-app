b3ff1de97d69e833d39ed95eb15beb4f9e1dccc9
nkosea07 <nkosiphile@marytechenock.com>
2026-02-13 15:51:29 +0200

    fix location dto

--- backend/src/main/java/com/uzwide/WeatherApp/dto/request/LocationDTO.java
+++ backend/src/main/java/com/uzwide/WeatherApp/dto/request/LocationDTO.java
@@ -7,16 +7,12 @@ import lombok.Data;
 @Data
 public class LocationDTO {
 
-    @NotBlank(message = "City name is required")
     private String name;
 
-    @NotBlank(message = "Country code is required")
     private String country;
 
-    @NotNull(message = "Latitude is required")
     private Double latitude;
 
-    @NotNull(message = "Longitude is required")
     private Double longitude;
 
     private String displayName;
